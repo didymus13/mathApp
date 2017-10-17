953ec1f4644a091a9551e93cadffb1423abbf2d5
Stephane Doiron <didymus1313@gmail.com>
2017-10-17 19:44:49 -0400

    layout fix

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -1,34 +1,35 @@
 <template>
-  <div id="config">
-    <div class="field is-horizontal">
-      <label for="minimum" class="label">Minimum</label>
-      <div class="control">
-        <input type="number" class="input" name="minimum" id="minimum" @input="updateMin" :value="config.numberMin">
+  <section class="section" id="config">
+    <div class="container">
+      <div class="field is-horizontal">
+        <label for="minimum" class="label">Minimum</label>
+        <div class="control">
+          <input type="number" class="input" name="minimum" id="minimum" @input="updateMin" :value="config.numberMin">
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <label for="maximum" class="label">Maximum</label>
+        <div class="control">
+          <input type="number" class="input" name="maximum" id="maximum" @input="updateMax" :value="config.numberMax">
+        </div>
+      </div>
+
+      <div class="field">
+        <input type="checkbox" name="useTotalForLimit" id="useTotalForLimit" class="switch" v-model="config.useTotalForLimit">
+        <label for="useTotalForLimit">useTotalForLimit</label>
+      </div>
+
+      <div class="field">
+        <input type="checkbox" name="wholeAnswersOnly" id="wholeAnswersOnly" class="switch" v-model="config.wholeAnswersOnly">
+        <label for="wholeAnswersOnly">wholeAnswersOnly</label>
+      </div>
+
+      <div class="field">
+        <input type="checkbox" name="positiveAnswersOnly" id="positiveAnswersOnly" class="switch" v-model="config.positiveAnswersOnly">
+        <label for="positiveAnswersOnly">positiveAnswersOnly</label>
       </div>
     </div>
-    <div class="field is-horizontal">
-      <label for="maximum" class="label">Maximum</label>
-      <div class="control">
-        <input type="number" class="input" name="maximum" id="maximum" @input="updateMax" :value="config.numberMax">
-      </div>
-    </div>
-
-    <div class="field">
-      <input type="checkbox" name="useTotalForLimit" id="useTotalForLimit" class="switch" v-model="config.useTotalForLimit">
-      <label for="useTotalForLimit">useTotalForLimit</label>
-    </div>
-
-    <div class="field">
-      <input type="checkbox" name="wholeAnswersOnly" id="wholeAnswersOnly" class="switch" v-model="config.wholeAnswersOnly">
-      <label for="wholeAnswersOnly">wholeAnswersOnly</label>
-    </div>
-
-    <div class="field">
-      <input type="checkbox" name="positiveAnswersOnly" id="positiveAnswersOnly" class="switch" v-model="config.positiveAnswersOnly">
-      <label for="positiveAnswersOnly">positiveAnswersOnly</label>
-    </div>
-
-  </div>
+  </section>
 </template>
 
 <script>
