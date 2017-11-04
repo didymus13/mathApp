4ac05cd423b51a087e5f404cf3e8c273b1da02ce
Stephane Doiron <didymus1313@gmail.com>
2017-11-04 10:50:21 -0400

    navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+
 export default {
+  components: {
+    Navbar
+  },
   name: 'app'
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 import Config from '@/components/Config'
 import Addition from '@/components/Addition'
@@ -10,11 +9,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
-    },
     {
       path: '/config',
       name: 'Config',
